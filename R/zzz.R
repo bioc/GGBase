6cc7b4e652e48aa68503b4f76c7a376aabe7a85c
Vince Carey <stvjc@channing.harvard.edu>
2008-05-19 16:39:06 +0000

    mods to synch with GGdata infrastructure
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_2/madman/Rpacks/GGBase@31776 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,25 +1,25 @@
 datacache <- new.env(hash=TRUE, parent=emptyenv())
 
-hmceuAmbB36_dbconn <- function() dbconn(datacache) # lexical scope?
-hmceuAmbB36_dbfile <- function() dbfile(datacache)
+hmceuAmbB36_23a_dbconn <- function() dbconn(datacache) # lexical scope?
+hmceuAmbB36_23a_dbfile <- function() dbfile(datacache)
 
 .onLoad <- function(libname, pkgname)
 {
     require("methods", quietly=TRUE)
     ## Connect to the SQLite DB
-    dbfile <- system.file("extdata", "hmceuAmbB36.sqlite", 
+    dbfile <- system.file("extdata", "hmceuAmbB36_23a.sqlite", 
        package=pkgname, lib.loc=libname)
     assign("dbfile", dbfile, envir=datacache)
     dbconn <- dbFileConnect(dbfile)
     assign("dbconn", dbconn, envir=datacache)
     ## Create the AnnObj instances
     aae = AnnotationDbi:::addToNamespaceAndExport
-    aae("hmceuAmbB36_dbconn", hmceuAmbB36_dbconn, pkgname)
-    aae("hmceuAmbB36_dbfile", hmceuAmbB36_dbfile, pkgname)
+    aae("hmceuAmbB36_23a_dbconn", hmceuAmbB36_23a_dbconn, pkgname)
+    aae("hmceuAmbB36_23a_dbfile", hmceuAmbB36_23a_dbfile, pkgname)
 }
 
 .onUnload <- function(libpath)
 {
-    dbFileDisconnect(hmceuAmbB36_dbconn())
+    dbFileDisconnect(hmceuAmbB36_23a_dbconn())
 }
 
