8ac636f5a4a50a4182a60a75f909fd54a4404cf1
Vince Carey <stvjc@channing.harvard.edu>
2009-03-07 23:20:58 +0000

    mods to featureFilter to kill the hap and qbl chroms
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGBase@37814 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/featureFilter.R
+++ R/featureFilter.R
@@ -9,17 +9,23 @@ featureFilter = function(x,
  if (!is(x, "smlSet")) stop("requires smlSet input")
  fn = featureNames(x)
  require(paste(clsuff(Biobase::annotation(x)), ".db", sep=""), character.only=TRUE, quietly=TRUE)
- basic = keys(get(paste(clsuff(Biobase::annotation(x)), "CHRLOC", sep="")))
+ clocOBJ = get(paste(clsuff(Biobase::annotation(x)), "CHRLOC", sep=""))
+ cOBJ = get(paste(clsuff(Biobase::annotation(x)), "CHR", sep=""))
+ basic = keys(clocOBJ)
  bad = which(!(fn %in% basic))
  fn = fn[-bad]
  if ("loc" %in% requires) {
-    locl = mget(fn, get(paste(clsuff(Biobase::annotation(x)), "CHRLOC", sep="")))
-    locl = sapply(locl , "[", 1)
+    locl = mget(fn, clocOBJ, ifnotfound=NA)
+    locln = lapply(locl, names)
+    for (i in 1:length(locln)) {  # kick out hap/qbl references
+       locl[[i]] = locl[[i]][ nchar(locln[[i]]) < 3 ]
+    }
+    locl = sapply(locl , "[", 1)  # take first qualifying location
     bad = which(is.na(locl))
     fn = fn[-bad]
     }
  if ("autosomal" %in% requires){
-    cn = mget(fn, get(paste(clsuff(Biobase::annotation(x)), "CHR", sep="")))
+    cn = mget(fn, cOBJ, ifnotfound=NA)
     cn = sapply(cn, "[", 1)
     bad = which(!(as.character(cn) %in% as.character(1:22)))
     fn = fn[-bad]
