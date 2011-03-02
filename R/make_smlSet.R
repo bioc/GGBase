095d6381e9bf93bc33f1eaaff935a6a51c0cac11
Vince Carey <stvjc@channing.harvard.edu>
2011-03-02 21:07:02 +0000

    remove references to snpMatrix2
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGBase@53371 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/make_smlSet.R
+++ R/make_smlSet.R
@@ -1,7 +1,7 @@
 
 make_smlSet = function(es, sml, organism="Homo sapiens") {
  if (!inherits(es, "ExpressionSet")) stop("es must be ExpressionSet instance")
- if (!inherits(sml[[1]], "SnpMatrix")) stop("sml must be list of SnpMatrix instances from 'snpMatrix2' package")
+ if (!inherits(sml[[1]], "SnpMatrix")) stop("sml must be list of SnpMatrix instances from 'snpStats' package")
  if (is.null(names(sml))) stop("sml must be named list [typically list elements are named '1', '2', ... enumerating chromosomes, could just be 'all'")
  smlenv = new.env()
  assign("smList", sml, envir=smlenv)
