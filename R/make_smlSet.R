5743bc912fd3f560040944d303492b959d3aa81d
Vince Carey <stvjc@channing.harvard.edu>
2011-03-09 11:54:06 +0000

    added harmonizeSamples option to make_smlSet
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GGBase@53536 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/make_smlSet.R
+++ R/make_smlSet.R
@@ -1,8 +1,20 @@
 
-make_smlSet = function(es, sml, organism="Homo sapiens") {
+make_smlSet = function(es, sml, organism="Homo sapiens", harmonizeSamples=FALSE) {
  if (!inherits(es, "ExpressionSet")) stop("es must be ExpressionSet instance")
  if (!inherits(sml[[1]], "SnpMatrix")) stop("sml must be list of SnpMatrix instances from 'snpStats' package")
  if (is.null(names(sml))) stop("sml must be named list [typically list elements are named '1', '2', ... enumerating chromosomes, could just be 'all'")
+ if (harmonizeSamples) {
+  esn = sampleNames(es)
+  ssn = rownames(sml[[1]]) # assumed common along list
+  if (!all(esn %in% ssn)) {
+   warning("harmonizeSamples TRUE and sampleNames for es not coincident with rownames(sml[[1]]); harmonizing...")
+   sn = intersect(esn, ssn)
+   es = es[,sn]
+   ns = names(sml)
+   sml = lapply(sml, function(x)x[sn,])
+   names(sml) = ns
+   } 
+ }  # otherwise validObject will get it
  smlenv = new.env()
  assign("smList", sml, envir=smlenv)
  new("smlSet", smlEnv=smlenv, annotation=annotation(es), organism=organism,
